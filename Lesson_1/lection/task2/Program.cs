b5fcc9db29e10e8b5959e6a5689f107b2d765ed4
Pavel <gordey8823@yandex.ru>
2023-12-20 14:50:37 +0500

    fix lesson1:task1, add lesson1:task2

---
+++ Lesson_1/lection/task2/Program.cs
@@ -0,0 +1,47 @@
+﻿// Пример 2 ------------------------------------------------
+Console.WriteLine("Сложение----------------");
+int i1 = 5;
+int i2 = 6;
+
+int sum = i1 + i2;
+Console.WriteLine("Сложение 1");
+Console.WriteLine(sum);
+
+long l1 = 6;
+long sum2 = l1 +i1;
+Console.WriteLine("Сложение 2");
+Console.WriteLine(sum2);
+
+double d1 = 5.5;
+double sum3 = i1 + d1;
+Console.WriteLine("Сложение 3");
+Console.WriteLine(sum3);
+
+Console.WriteLine("Умножение----------------");
+
+Console.WriteLine("Умножение 1");
+int product1 = i1 * i2;
+Console.WriteLine(product1);
+
+Console.WriteLine("Умножение 2");
+long product2 = l1 * i1;
+Console.WriteLine(product2);
+
+Console.WriteLine("Умножение 3");
+double product3 = i1 * d1;
+Console.WriteLine(product3);
+
+Console.WriteLine("Деление----------------");
+
+d1 = 6;
+Console.WriteLine("Деление 1");
+int quotient1 = i1 / i2;
+Console.WriteLine(quotient1);
+
+Console.WriteLine("Деление 2");
+double quotient2 = i1 / d1;
+Console.WriteLine(quotient2);
+
+Console.WriteLine("Деление 3");
+double quotient3 = i1 % d1;
+Console.WriteLine(quotient3);
