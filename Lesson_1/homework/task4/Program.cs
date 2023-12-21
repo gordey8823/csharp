64f617838127212400a865fbc81ea3d8c0bd7c8c
Pavel <gordey8823@yandex.ru>
2023-12-21 16:48:35 +0500

    add tasks lesson1:homework

---
+++ Lesson_1/homework/task4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Answer {
+    static void PrintEvenNumbers(int number)
+    {
+      // Решение
+      for(int i = 2; i <= number; i += 2)
+      {
+        Console.Write(i);
+        Console.Write('\t');
+      }
+    
+      // Конец решения
+    }
+
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 6;
+        }
+
+        // Не удаляйте строки ниже
+        PrintEvenNumbers(number);
+    }
+}
