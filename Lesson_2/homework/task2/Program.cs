619393895a32626a23bf3724d3345088fcbde512
Pavel <gordey8823@yandex.ru>
2023-12-25 15:52:57 +0500

    add lesson2:hw:tasks

---
+++ Lesson_2/homework/task2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите координату по оси X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату по оси Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if(x > 0 && y > 0)
+{
+    Console.Write($"Точка {x}:{y} находится в первой четверти");
+    return;
+}
+else if(x < 0 && y > 0)
+{
+    Console.Write($"Точка {x}:{y} находится во второй четверти");
+    return;
+}
+else if(x < 0 && y < 0)
+{
+    Console.Write($"Точка {x}:{y} находится в третьей четверти");
+    return;
+}
+else if(x > 0 && y < 0)
+{
+    Console.Write($"Точка {x}:{y} находится в четвертой четверти");
+    return;
+}
+
---
+++ Lesson_2/homework/task4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите натуральное число N: ");
+
+string number = Console.ReadLine();
+
+int numberLength = number.Length;
+
+if (Convert.ToInt32(number) < 10)
+{
+    Console.Write(number);
+}
+else
+{
+    for(int i = 0; i < numberLength; i++)
+    {
+        Console.Write(number[i]);
+        Console.Write(", ");
+    }
+    Console.Write(number[numberLength - 1]);
+}
