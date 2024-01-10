7df708ba6ac69b25f3e514a3cbd6fbc613fea43c
Pavel <gordey8823@yandex.ru>
2024-01-10 13:43:42 +0500

    add tasks from lesson3:seminar

---
+++ Lesson_3/seminar/task3/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 2, 3, 1, 7, 5, 6, 3 };
+int[] arrayRezult = new int[array.Length / 2];
+int back = array.Length - 1;
+
+
+
+for (int i = 0; i < array.Length / 2; i++)
+{
+    arrayRezult[i] = array[i] * array[back - i];
+}
+
+foreach (int n in arrayRezult)
+{
+    Console.Write(n + " ");
+}
