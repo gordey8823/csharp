d7fd2b9dd674d7d210323fc0dba2d1f81e191537
Pavel <gordey8823@yandex.ru>
2024-01-10 11:25:10 +0500

    add lesson3:lection tasks

---
+++ Lesson_3/lection/task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// int n = 10;
+int[] arr = { 2, 5, 4, 7, 8, 35, 1, 3, 8, 5 };
+int i = 0;
+
+Console.Write("Чётные: ");
+while (i < arr.Length)
+{
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+        
+    }
+    i += 1;
+}
