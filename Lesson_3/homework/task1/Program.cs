f570e370b36f15d5d5be8c82009dcc4e6f588ef5
Pavel <gordey8823@yandex.ru>
2024-01-11 10:30:03 +0500

    add tasks lesson3:hw

---
+++ Lesson_3/homework/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = { 10, 21, 14, 93, 23, 15, 17, 43, 97, 12 }; // 4
+int count = 0;
+
+foreach (int i in array)
+{
+    if (20 <= i && i <= 90)
+    {
+        count += 1;
+    }
+}
+
+Console.Write($"В промежутке от 20 до 90 целых чисел {count}");
---
+++ Lesson_3/homework/task4/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите натуральное число от 1 до 100000: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int numberCopy = number;
+int count = 0;
+while (numberCopy > 0)
+{
+    count += 1;
+    numberCopy /= 10;
+}
+
+int[] array = new int[count];
+
+for (int i = array.Length; i > 0; i--)
+{
+    array[i - 1] = number % 10;
+    number /= 10;
+}
+
+Console.Write("Знаки числа: ");
+foreach(int n in array)
+{
+   Console.Write($"{n} "); 
+}
