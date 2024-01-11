5e6ad5aca938e64ad965cde211092a24bc309c8b
Pavel <gordey8823@yandex.ru>
2024-01-11 14:16:05 +0500

    fix tasks lesson3:hw

--- Lesson_3/homework/task2/Program.cs
+++ Lesson_3/homework/task2/Program.cs
@@ -1,12 +1,56 @@
-﻿int[] array = { 10, 21, 14, 93, 23, 15, 17, 43, 97, 12 }; //3
-int count = 0;
-
-foreach (int i in array)
+﻿//Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
+class UserInputToCompileForTest
 {
-    if (i % 2 == 0)
+    // Подсчет количества четных элементов массива
+    // numbers - массив, в котором ведется подсчет
+    public static int CountEvenItems(int[] numbers)
     {
-        count += 1;
+        //Напишите свое решение здесь
+        int count = 0;
+
+        foreach (int i in numbers)
+        {
+            if (i % 2 == 0)
+            {
+                count += 1;
+            }
+        }
+        return count;
+    }
+
+    public static void PrintResult(int[] array)
+    {
+        //Напишите свое решение здесь
+        Console.WriteLine(CountEvenItems(array));
     }
 }
 
-Console.Write($"Количество в массиве целых чётных чисел {count}");
+//Не удаляйте и не меняйте класс Answer!
+class Answer
+{
+    public static void Main(string[] args)
+    {
+        int[] array;
+
+
+        if (args.Length >= 1)
+        {
+            // Объединяем все аргументы командной строки в одну строку
+            string joinedArgs = string.Join(" ", args);
+
+            // Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
+            array = joinedArgs.Split(", ")
+                                  .Select(int.Parse)
+                                  .ToArray();
+
+            // Теперь arr содержит преобразованные в целые числа из командной строки
+
+        }
+        else
+        {
+            // Если аргументов на входе нет
+            array = new int[] { 2, 5, 2, 3, 6, 3, 7, 8, 2, 4 }; // Создание массива
+        }
+        UserInputToCompileForTest.PrintResult(array);
+    }
+}
--- Lesson_3/homework/task3/Program.cs
+++ Lesson_3/homework/task3/Program.cs
@@ -1,18 +1,97 @@
-﻿double[] array = { 2.2, 0.4, 9.11, 7.2, 78.98 };
+﻿// double[] array = { 2.2, 0.4, 9.11, 7.2, 78.98 };
 
-double min = array[0];
-double max = array[0];
+// double min = array[0];
+// double max = array[0];
 
-foreach (double i in array)
+// foreach (double i in array)
+// {
+//     if (i > max)
+//     {
+//         max = i;
+//     }
+//     if (i < min)
+//     {
+//         min = i;
+//     }
+// }
+
+// Console.Write($"Разница между минимальным {min} и максимальным {max} числом равна {max - min}");
+
+using System;
+using System.Linq;
+
+//Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
+class UserInputToCompileForTest
 {
-    if (i > max)
+    // Разница между максимальным и минимальным элементами массива
+
+    // Нахождение минимума массива
+    public static double FindMin(double[] numbers)
     {
-        max = i;
+        //Напишите свое решение здесь
+        double min = numbers[0];
+
+        foreach (double i in numbers)
+        {
+            if (i < min)
+            {
+                min = i;
+            }
+        }
+        return min;
     }
-    if (i < min)
+
+    // Нахождение максимума массива
+    public static double FindMax(double[] numbers)
     {
-        min = i;
+        // Напишите свое решение здесь
+        double max = numbers[0];
+
+        foreach (double i in numbers)
+        {
+            if (i > max)
+            {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+
+
+    public static void PrintResult(double[] array)
+    {
+        //Напишите свое решение здесь
+        Console.WriteLine(FindMax(array) - FindMin(array));
     }
 }
 
-Console.Write($"Разница между минимальным {min} и максимальным {max} числом равна {max - min}");
+//Не удаляйте и не меняйте класс Answer!
+class Answer
+{
+    public static void Main(string[] args)
+    {
+        double[] array;
+
+
+        if (args.Length >= 1)
+        {
+            // Объединяем все аргументы командной строки в одну строку
+            string joinedArgs = string.Join(" ", args);
+
+            // Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
+            array = joinedArgs.Split(", ")
+                                  .Select(double.Parse)
+                                  .ToArray();
+
+            // Теперь arr содержит преобразованные в целые числа из командной строки
+
+        }
+        else
+        {
+            // Если аргументов на входе нет
+            array = new double[] { 0.25, 5.4, 1.3, 2.1, 3.8, 5.2, 3.01 }; // Создание массива
+        }
+        UserInputToCompileForTest.PrintResult(array);
+    }
+}
