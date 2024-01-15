96a3dfe94f351fd0e7201d19fdbc7dde4d0261e8
Pavel <gordey8823@yandex.ru>
2024-01-15 15:30:31 +0500

    add tasks Lesson4:hw

---
+++ Lesson_4/homework/task3/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] ReverseArray(int[] array)
+{
+    int secondPerem = 0;
+    for (int i = 0, j = array.Length - 1; i < array.Length / 2; i++, j--)
+    {
+        secondPerem = array[i];
+        array[i] = array[j];
+        array[j] = secondPerem;
+    }
+    return array;
+}
+
+int[] arrayN = { 1, 2, 3, 4, 5, 6 };
+
+string str1 = string.Join(", ", arrayN);
+string str2 = string.Join(", ", ReverseArray(arrayN));
+
+Console.Write($"Массив [{str1}] перевернем и получим [{str2}]");
